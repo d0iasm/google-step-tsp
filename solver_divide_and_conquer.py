5c98554fe5fe71a12e9b0ad6e14616a4b2a1c517
Asami Doi <doiasami1219@gmail.com>
2017-06-27 20:35:22 +0900

    update divide method

--- solver_divide_and_conquer.py
+++ solver_divide_and_conquer.py
@@ -16,51 +16,42 @@ def is_x_longer(cities):
     return x2 - x1 > y2 - y1
 
 
-def devide(cities):
-    length = len(cities)
-    middle = int(length/2)
-    if length <= 3:
-        print(cities)
+def divide(cities):
+    N = len(cities)
+    if N <= 3:
         return cities
 
+    middle = int(N/2)
     if is_x_longer(cities):
-        print("xが長い")
-        print(sorted(cities, key=lambda x: x[1]))
-        devide(cities[:middle])
-        devide(cities[middle:])
+        cities = sorted(cities, key=lambda x: x[0])
     else:
-        print("yが長い")
-        print(sorted(cities, key=lambda x: x[0]))
-        devide(cities[:middle])
-        devide(cities[middle:])
+        cities = sorted(cities, key=lambda x: x[1])
+    cities1 = divide(cities[:middle])
+    cities2 = divide(cities[middle:])
 
+    return merge(cities1, cities2)
+
+
+def merge(cities1, cities2):
+    assert cities1 is not None, "error cities1 is None"
+    assert cities2 is not None, "error cities2 is None"
+
+    return cities1 + cities2
 
 
 def solve(cities):
     N = len(cities)
-    devide(cities)
+    index = [i for i in range(N)]
+    index_cities = dict(zip(cities, index))
+    sorted_cities = divide(cities)
 
-    # dist = [[0] * N for i in range(N)]
-    # for i in range(N):
-    #     for j in range(N):
-    #         dist[i][j] = dist[j][i] = distance(cities[i], cities[j])
-    #
-    # current_city = 0
-    # unvisited_cities = set(range(1, N))
-    # solution = [current_city]
-    #
-    # def distance_from_current_city(to):
-    #     return dist[current_city][to]
-    #
-    # while unvisited_cities:
-    #     next_city = min(unvisited_cities, key=distance_from_current_city)
-    #     unvisited_cities.remove(next_city)
-    #     solution.append(next_city)
-    #     current_city = next_city
-    # return solution
+    solution = []
+    for city in sorted_cities:
+        solution.append(index_cities[city])
+    return solution
 
 
 if __name__ == '__main__':
     assert len(sys.argv) > 1
     solution = solve(read_input(sys.argv[1]))
-    # print_solution(solution)
+    print_solution(solution)
