29656e7e1c3a01923c34a255663a5a7db85e87f8
Asami Doi <doiasami1219@gmail.com>
2017-06-28 15:16:28 +0900

    update 2-opt method

--- solver_optimize.py
+++ solver_optimize.py
@@ -10,24 +10,29 @@ def distance(city1, city2):
     return math.sqrt((city1[0] - city2[0]) ** 2 + (city1[1] - city2[1]) ** 2)
 
 
-def opt_2(solution, dist):
-    N = len(solution)
-    print(solution)
-    for i, current in enumerate(solution[:(N-1)]):
-        # print("current:{}, current+1:{}".format(current, solution[i+1]))
-        best_distance = dist[current][solution[i+1]]
-        print("{}".format(best_distance))
-        for to in solution[(i+1):]:
-        # for to in range(i+1, N):
-            print("{}, {} : dist{}".format(current, to, dist[current][to]))
-            new_distance = dist[current][to]
-            if new_distance < best_distance:
-                temp = solution[current]
-                solution[current] = solution[to]
-                solution[to] = temp
-                best_distance = new_distance
-                print("swap! c:{}, to:{}".format(current,solution[to]))
-    return solution
+def opt_2(N, dist):
+    path = [i for i in range(N)]
+    while True:
+        count = 0
+        for i in range(N - 2):
+            i1 = i + 1
+            for j in range(i + 2, N):
+                if j == N - 1:
+                    j1 = 0
+                else:
+                    j1 = j + 1
+                if i != 0 or j1 != 0:
+                    l1 = dist[path[i]][path[i1]]
+                    l2 = dist[path[j]][path[j1]]
+                    l3 = dist[path[i]][path[j]]
+                    l4 = dist[path[i1]][path[j1]]
+                    if l1 + l2 > l3 + l4:
+                        new_path = path[i1:j+1]
+                        path[i1:j+1] = new_path[::-1]
+                        count += 1
+        if count == 0: break
+    return path
+
 
 def or_opr():
     pass
@@ -41,19 +46,7 @@ def solve(cities):
         for j in range(N):
             dist[i][j] = dist[j][i] = distance(cities[i], cities[j])
 
-    current_city = 0
-    unvisited_cities = set(range(1, N))
-    solution = [current_city]
-
-    def distance_from_current_city(to):
-        return dist[current_city][to]
-
-    while unvisited_cities:
-        next_city = min(unvisited_cities, key=distance_from_current_city)
-        unvisited_cities.remove(next_city)
-        solution.append(next_city)
-        current_city = next_city
-    return opt_2(solution, dist)
+    return opt_2(N, dist)
 
 
 if __name__ == '__main__':
