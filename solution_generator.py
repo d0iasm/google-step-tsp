5b967f355643b754e5cb91df54a7097a5752104b
Asami Doi <doiasami1219@gmail.com>
2017-06-28 16:08:54 +0900

    update optimize solutions of divide and conquer

--- solution_generator.py
+++ solution_generator.py
@@ -3,7 +3,7 @@
 import sys
 import math
 
-from common import format_solution, read_input
+from common import format_solution, read_input, read_solution
 
 import solver_greedy
 import solver_random
@@ -15,12 +15,13 @@ CHALLENGES = 7
 def generate_sample_solutions():
     # solvers = ((solver_random, 'random'),
             #    (solver_greedy, 'greedy'))
-    solvers = ((solver_random, 'random'),
-                (solver_optimize, 'yours'))
+    solvers = ((solver_optimize, 'yours'),)
     for challenge_number in range(CHALLENGES):
         cities = read_input('input/{}.csv'.format(challenge_number))
+        other_solution = read_solution('solution_yours/{}_divide.csv'.format(challenge_number))
         for solver, solver_name in solvers:
-            solution = solver.solve(cities)
+            # solution = solver.solve(cities)
+            solution = solver.solve(cities, other_solution)
             with open('solution_{}/{}.csv'.format(solver_name, challenge_number), 'w') as f:
                 f.write(format_solution(solution) + '\n')
 
